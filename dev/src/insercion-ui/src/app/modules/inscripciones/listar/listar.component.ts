222a67bb5018d96605b56e7e5ec97f9a7c380a73
walterblanco <walter@creativehands.com.ar>
2019-08-26 16:27:00 -0300

    contino estilando y maquetando las pantalla de insecion

--- dev/src/insercion-ui/src/app/modules/inscripciones/listar/listar.component.ts
+++ dev/src/insercion-ui/src/app/modules/inscripciones/listar/listar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { InsercionlaboralService } from 'src/app/shared/insercionlaboral.service';
+import {SelectionModel} from '@angular/cdk/collections';
+import { filter, switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-listar',
@@ -10,13 +13,32 @@ import { InsercionlaboralService } from 'src/app/shared/insercionlaboral.service
 export class ListarComponent implements OnInit {
 
   inscripciones$: Observable<any>;
+  inscripciones_filtradas$: Observable<any>;
+  selection: SelectionModel<any>;
+  f$ = new BehaviorSubject<void>(null);
+
+  _columnas = ['postulante','carrera','fecha','oferta','materias','promedio','edad','residencia','experiencia','acciones']
+
+  columnas() {
+    return this._columnas;
+  }
 
   constructor(service: InsercionlaboralService) { 
     let uid = '';
+    this.selection = new SelectionModel<any>(true, []);
     this.inscripciones$ = service.obtenerInscripciones(uid);
+    this.inscripciones_filtradas$ = this.f$.pipe(
+      switchMap(_ => this.inscripciones$.pipe(
+        filter(n => !this.selection.isSelected(n))
+      ))
+    );
   }
 
   ngOnInit() {
+    this.selection.changed.subscribe(x => this.f$.next());
+    this.f$.next();
   }
+
+  
 
 }
