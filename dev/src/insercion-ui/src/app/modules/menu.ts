c990109d2889a93c6ffa9033e52b95f14f4752f3
walterblanco <walter@creativehands.com.ar>
2019-12-27 13:31:38 +0000

    -Agregue un dialogo en la pantalla de selecciones

--- dev/src/insercion-ui/src/app/modules/menu.ts
+++ dev/src/insercion-ui/src/app/modules/menu.ts
@@ -7,6 +7,8 @@ export const menu : MenuSistema = [
     // { item: 'Inscribirme', menu: null, ruta: '/sistema/inscripciones/crear', icono: 'add_circle_outline', permisos: [] },
 
     // esto es de insercion laboral para administrar
-    { item: 'Inscripciones', menu: null, ruta: '/sistema/inscripciones/listar', icono: 'people_outline', permisos: [] }
+    { item: 'Inscripciones', menu: null, ruta: '/sistema/inscripciones/listar', icono: 'people_outline', permisos: [] },
+    { item: 'Selecciones', menu: null, ruta: '/sistema/selecciones/listar', icono: 'star_border', permisos: [] }
+
     
 ];
